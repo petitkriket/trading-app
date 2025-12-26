59eafda341edac1dc83cd45cfa5d275f02eb5a73
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2025-12-26 10:47:44 +0100

    chore(eslint): stricter linter rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -19,13 +19,13 @@ export default defineConfigWithVueTs(
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
   {
     ...pluginCypress.configs.recommended,
     files: [
       '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+      'cypress/tests/**/*.{cy,spec}.{js,ts,jsx,tsx}',
       'cypress/support/**/*.{js,ts,jsx,tsx}',
     ],
   },
